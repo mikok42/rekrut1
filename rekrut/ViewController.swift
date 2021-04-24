e445c5177c717fe5894053e547f77c2ae8089cf9
mikok42 <mikolaj@linczewski.com>
2021-04-24 18:25:15 +0200

    init

---
+++ rekrut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rekrut
+//
+//  Created by Mikołaj Linczewski on 24/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
